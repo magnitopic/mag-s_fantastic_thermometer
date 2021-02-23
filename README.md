d1636860c5545e4485805f8ace8280eaac03b933
magnitopic <magnitopic@gmail.com>
2021-02-24 00:02:19 +0100

    Added reedme and first .ino program

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mag-s_fantastic_thermometer
+# Mag's Fantastic Thermometer
+
+This is a project that involves two main technologies: Arduino and Web.
+
+The code that I have right now is being used in a ESP8266 board, NodeMCU V3 (ESP-12E) model, alongside the DHT22 temperature and humidity sensor.
+The ESP board creates a web server that shows a page where you can see the temperature and the humidity in real time.
+
+The final goal with this project is having the ESP board read data from different sensors, humidity, light, temperature... As many as I can add.
+Later, for that data to be sent in JSON format via an HTTP POST request to the [magnitopic.com NodeJS server](https://github.com/magnitopic/magnitopic.com-express) and stored in the MongoDB database.
+The [magnitopic.com](https://www.magnitopic.com/) website will later have a web page from which to show the changes in real time, and some graphs and charts to go along with it.
---
+++ client.ino
@@ -0,0 +1,57 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+const char *ssid = "";
+const char *password = "";
+
+//Some settings for the DHT22 sensor
+#define DHTTYPE DHT22
+
+    uint8_t DHTPin = D8;
+
+DHT dht(DHTPin, DHTTYPE);
+
+float Temperature;
+float Humidity;
+
+//We start the program
+void setup(){
+  Serial.begin(115200);
+
+  pinMode(DHTPin, INPUT);
+
+  dht.begin();
+
+  //Connect to the wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check if we're connected to the wi-fi network
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+}
+
+void loop(){
+
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity();       // Gets the values of the humidity
+
+  HTTPClient http; //Declare object of class HTTPClient
+
+  http.begin("http://192.168.1.88:8085/hello"); //Specify request destination
+  http.addHeader("Content-Type", "text/plain"); //Specify content-type header
+
+  int httpCode = http.POST("Message from ESP8266"); //Send the request
+  String payload = http.getString();                //Get the response payload
+
+  Serial.println(httpCode); //Print HTTP return code
+  Serial.println(payload);  //Print request response payload
+
+  http.end(); //Close connection
+
+  delay(30000); //Send a request every 30 seconds
+}
