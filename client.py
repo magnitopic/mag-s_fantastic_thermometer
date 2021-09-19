4cbef983fb6cb7fe6a46b1a4ffc4c33a9075be7a
magnitopic <magnitopic@gmail.com>
2021-09-19 19:18:46 +0200

    client.py makes periodic requests

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 from sense_hat import SenseHat
-import requests
+import schedule, requests
 
 sense = SenseHat()
 
 def newRequest():
-	pload = {"temperature":str(round(sense.temp)),"humidity":str(round(sense.humidity))}
+	pload = {"temperature":str(round(sense.temp,1)),"humidity":str(round(sense.humidity,1))}
 	r = requests.post('https://limitless-refuge-99811.herokuapp.com/esp',data = pload)
 	print(r.text)
 
 newRequest()
+if __file__=='__main___':
+	schedule.every(60).minutes.do(newRequest)
